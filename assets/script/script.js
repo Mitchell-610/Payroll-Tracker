8f996c91dd1bee7baf8436ef7984f645c49ccc8c
Mitchell-610 <Mheit602@gmail.com>
2024-06-03 22:14:21 -0400

    Let for our while loop added.

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,6 +5,8 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 const collectEmployees = function () {
   // TODO: Get user input to create and return an array of employee objects
   const employees = [];
+  let addingEmployees = true;
+
 
 }
 
